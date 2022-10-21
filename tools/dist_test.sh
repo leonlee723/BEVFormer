32e1988c4521dfedbf99e1978e1b96e11855f6b4
leonlee723 <leonlee723@gmail.com>
2022-10-21 18:58:53 +0800

    update launch.json

--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -4,7 +4,9 @@ CONFIG=$1
 CHECKPOINT=$2
 GPUS=$3
 PORT=${PORT:-29503}
-
+echo $(dirname "$0")
+echo ${@:3}
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+echo $PYTHONPATH
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     $(dirname "$0")/test.py $CONFIG $CHECKPOINT --launcher pytorch ${@:4} --eval bbox
